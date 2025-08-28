2b7d7d0f2fa1c7f3a873053986e084eea6fa153c
mrcallaghan <mcallaghan@naperville203.org>
2025-08-28 14:56:05 -0500

    Wrote ch 2 notes on primitives and objects

--- Chapter2.java
+++ Chapter2.java
@@ -33,5 +33,77 @@ public class Chapter2
          */
         
         Turtle crush = new Turtle(ocean);
+        
+        /*
+         *  Varibales store values to be used later in program.
+         *          Variables have a type (e.g., int or Turtle)
+         *                         a name (e.g., width or crush)
+         *                         a value (e.g., 20 or a Turtle object reference)
+         */
+        int width;  // declare a variable with a type and name
+        width = 20; // assign a value to a variable
+        int xPosition = 50;  // both declare and assign in one line
+        
+        /*
+         * Java has several type of primitive data types:
+         *      - boolean holds a true or false
+         *      - int holds an integer (whole number)
+         *      - double holds a real number (floating point or decimals)
+         *      - char holds a single character
+         */
+        
+        // boolean
+        boolean isSummer = true;
+        
+        // double
+        double salesTaxRate = 0.0775;
+        
+        // char
+        char letter = 'c';
+        char question = '?';
+        
+        /*
+         * When invoking methods, we use the dot operator (e.g., '.') to invoke a method on an object.
+         * 
+         * Some methods take no arguemnts, but we still need parentheses. (e.g., penDown)
+         * 
+         * Some methods take one or more arguments (e.g., forward).  Multiple arguments are separated by commas.
+         * 
+         * Mutator methods modify the state of the object (i.e., values of the attributes).
+         *      e.g., forward, turnLeft, setPostion
+         */
+        crush.forward(100);
+        crush.penUp();
+        crush.turn(90);
+        crush.forward(100);
+        crush.penDown();
+        crush.forward(100);
+        crush.turnLeft();
+        
+        /*
+         * Accessor methods return the value of an attribute of the object.
+         *      The state of the object doesn't change.
+         *      
+         *      e.g., getPenWidth, getXPos
+         */
+        
+        int penWidth = crush.getPenWidth();
+        
+        
+        System.out.println("The current pen width of crush is: " + penWidth);
+        
+        System.out.println("The current pen width of crush is: " + penWidth);
+        
+        System.out.print("The current pen width of crush is: " + penWidth);
+        
+        System.out.print("The current pen width of crush is: " + penWidth);
+        
+        
+        
+        
+        
+        
+        
+        
     }
 }
