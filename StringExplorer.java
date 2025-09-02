c3a9dd1ffaf01d0dc06341ac9f9a7a7c900d334a
mrcallaghan <mcallaghan@naperville203.org>
2025-09-02 08:29:29 -0500

    Started Ch 2 notes on Strings

--- StringExplorer.java
+++ StringExplorer.java
@@ -2,13 +2,49 @@
 /**
  * Exploring the String class and its methods.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author mrcallaghan
+ * @version 02sep2025
  */
 public class StringExplorer
 {
     public static void main(String[] args)
     {
+        /*
+         * Strings are objects that represent a sequence of characters.
+         *      Surround a string with doble quotes, just like in Python.
+         * Unlike Python:
+         *      - single quotes are reserved for the char primitive type
+         *      - no triple-quoted strings
+         *      - no f-strings
+         *      - Strings can be concatenated with other types of things (e.g., int) and type conversion occurs
+         */
+        
+        String river = new String("Mississippi");
+        System.out.println(river);
+        
+        /*
+         * Since Strings are so common, a shortcut way to construct them is provided called a String literal.
+         *      Same as we did in Python.
+         */
+        String river2 = "Nile";
+        System.out.println(river2);
+        
+        /*
+         *  Search for "javadoc String" to find the official String documentation.
+         */
+        river.replace("i", "x");
+        System.out.println(river);
+        
+        /*
+         * Challenge:  Fix String to actually replace 'i' with 'x'.
+         */
         
     }
+    
+    
+    
+    
+    
+    
+    
 }
